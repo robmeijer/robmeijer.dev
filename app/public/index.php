11fd787548c3f9f9bbb9e1f90f7bfc7f770ab950
Rob Meijer <profusion@gmail.com>
2024-12-15 10:46:05 +0000

    Enable support for the Fly Proxy

--- app/public/index.php
+++ app/public/index.php
@@ -4,6 +4,11 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+/* This is needed to support the Fly Proxy which uses the custom 'Fly-Client-IP' header */
+if (isset($_SERVER['HTTP_FLY_CLIENT_IP'])) {
+    $_SERVER['HTTP_X_FORWARDED_FOR'] = $_SERVER['HTTP_FLY_CLIENT_IP'];
+}
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
